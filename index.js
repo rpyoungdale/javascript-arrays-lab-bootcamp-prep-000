72d8e4093f124caf3ed871b2fde483aa7861710f
Ryan Youngdale <youngdale95@gmail.com>
2018-03-03 19:59:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ function destructivelyAppendKitten(name) {
 function destructivelyPrependKitten(name) {
   return kittens.unshift(name);
 }
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
