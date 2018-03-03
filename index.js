ade37ce556895389ed2a180ef243171ddc0d39a0
Ryan Youngdale <youngdale95@gmail.com>
2018-03-03 20:07:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ function appendKitten(name) {
 function prependKitten(name) {
   return [name, ...kittens];
 }
-function removeFirstKitten() {
-  kittens.slice(-1);
-  return kittens;
+function removeLastKitten() {
+  return kittens.slice(-1);
 }
